9b552abf50da2cd67907d1fcac171fee962a8902
Scott Polhemus <scott.polhemus@barrelny.com>
2013-08-01 18:17:56 -0400

    fix moveNPCs() call

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -747,6 +747,7 @@
 						
 			};
 					
+			game.moveNPCs();
 		},
 		
 		dropMoveable: function(objectName, shuntDir){
@@ -844,7 +845,6 @@
 			
 			
 			console.info(game.collisionMatrix);
-			game.moveNPCs();
 		},
 
 		/*
